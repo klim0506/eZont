4838e43856bb1e8d0244eb4e1b991e146e9dd1e3
Klimenty <klim.shchegolkov@mail.ru>
2022-04-17 23:45:39 +0300

    ready all fun in YC API (find near device, map of near device, and map of all devises)

--- files/API_maps.py
+++ files/API_maps.py
@@ -1,19 +1,66 @@
 # В этом файле собраны функции и классы для работы с Яндекс Картами
+import sqlite3
+import requests
+import numpy
+
 
 # Определение ближайшего аппарата
-def near_app():
-    pass
+def near_app(path_to_db, latitude, longitude):
+    # Получаем список координат всех аппаратов из БД
+    connect = sqlite3.connect(path_to_db)
+    cursor = connect.cursor()
+
+    coordinates = list(map(lambda x: tuple(float(i) for i in x[0].split(', ')),
+                           cursor.execute("SELECT coordinate FROM points").fetchall()))
+
+    # Находим ближайшую координату к местоположению пользователя
+    coordinates = numpy.array(coordinates)
+    user_location = numpy.array((longitude, latitude))
+    nearst_dev_coord = coordinates[numpy.argmin(numpy.linalg.norm(coordinates-user_location, axis=1))]
+
+    # Возращаем данные об аппарате из БД
+    info = cursor.execute(f"SELECT place, free_cell, fill_cell FROM points WHERE coordinate='{nearst_dev_coord[0]}, {nearst_dev_coord[1]}'").fetchall()[0]
+
+    data = {'place': info[0],
+            'latitude': nearst_dev_coord[0],
+            'longitude': nearst_dev_coord[1],
+            'free_cell': info[1],
+            'fill_cell': info[2]}
+
+    return data
 
 
 # Карта-схема до ближайшего аппарата
-def near_app_map():
-    pass
+def near_app_map(user_coord, device_coord):
+    response = requests.get(f"https://static-maps.yandex.ru/1.x/?pt={user_coord[1]},{user_coord[0]},"
+                            f"org~{device_coord[1]},{device_coord[0]},round&l=map")
 
-
-def all_app():
-    pass
+    with open('images/near_app_map.jpg', 'wb') as file:
+        file.write(response.content)
 
 
 # Карта всех аппаратов из БД
-def all_app_map():
-    pass
+def all_app_map(path_to_db):
+    url = 'https://static-maps.yandex.ru/1.x/?l=map&pt='
+
+    connect = sqlite3.connect(path_to_db)
+    cursor = connect.cursor()
+    all_coordinates = list(map(lambda x: tuple(float(i) for i in x[0].split(', ')),
+                               cursor.execute("SELECT coordinate FROM points").fetchall()))
+
+    for point in all_coordinates:
+        url = url + str(point[1]) + ',' + str(point[0]) + ',round' + '~'
+
+    response = requests.get(url[:-1])
+
+    with open('images/all_app_map.jpg', 'wb') as file:
+        file.write(response.content)
+
+
+if __name__ == '__main__':
+    longitude = 55.662627
+    latitude = 37.476347
+
+    my_dev = near_app('../db/data.db', longitude=longitude, latitude=latitude)
+    near_app_map((longitude, latitude), (my_dev['latitude'], my_dev['longitude']))
+    all_app_map('../db/data.db')
--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-# from const import *  # здесь находятся все коды, их не выкладываем на гит хаб, передаем в дискорде
-import logging
+from const import *  # здесь находятся все коды, их не выкладываем на гит хаб, передаем в дискорде
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 from telegram import ReplyKeyboardMarkup
-import requests
-import sys
-import os
+from files.API_maps import *
 
 start_keyboard = [['/find_dev', '/take_umbrella', '/return_umbrella'], ['/about_us', '/help_me']]
-markup = ReplyKeyboardMarkup(start_keyboard, one_time_keyboard=False)
+markup = ReplyKeyboardMarkup(start_keyboard, one_time_keyboard=True)
 
 
 def start(update, context):
@@ -29,37 +26,13 @@ def return_umbrella(update, context):
 
 # Найти ближайший аппарат и вывести фото
 def find_dev(update, context):
-    pass
+    print(123)
+    print(update.message.location)
 
 
 # Вывести фото катры-схемы всех аппаратов
 def all_devs(bot, context):
-    coords = bot.message.location
-    coords_n = [coords['longitude'], coords['latitude']]
-
-    url = "http://static-maps.yandex.ru/1.x"
-    params = {
-        "ll": '37.477935,55.663528',
-        "spn": "0.01,0.01",
-        "l": "map",
-        'pt': '37.482074,55.662759,pmdos~37.477911,55.661725,pmdos~37.479909,55.660584,pmdos'
-    }
-    params['pt'] = params['pt'] + '~' + str(coords_n[0]) + ',' + str(coords_n[1])
-    response = requests.get(url, params=params)
-
-    if not response:
-        print("Ошибка выполнения запроса:")
-        print("Http статус:", response.status_code, "(", response.reason, ")")
-        sys.exit(1)
-
-    map_file = "map.png"
-    with open(map_file, "wb") as file:
-        file.write(response.content)
-
-    context.bot.send_photo(chat_id=1740531612, photo=open(map_file, 'rb'))
-
-    # Удаляем за собой файл с изображением.
-    os.remove(map_file)
+    pass
 
 
 # Написать в поддержку
@@ -84,8 +57,6 @@ def take_instructions(update, context):
 
 
 def main():
-    updater = Updater(Token)
-    dp = updater.dispatcher
     dp.add_handler(CommandHandler("find_dev", find_dev))
     dp.add_handler(CommandHandler("take_umbrella", take_umbrella))
     dp.add_handler(CommandHandler("return_umbrella", return_umbrella))
@@ -105,6 +76,8 @@ def main():
 
 
 if __name__ == '__main__':
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
     main()
 
 # координаты
